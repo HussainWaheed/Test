ea7732d84068fe6a7aa92f31e1a6cbd0d96fe28e
Hussain Waheed Akhtar <hussainwaheedakhtar@outlook.com>
2015-11-19 15:01:18 +0500

    Initial Commit

---
+++ TestWebApp/Controllers/SessionController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace TestWebApp.Controllers
+{
+    public class ApplicationController:ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Session(SessionPostViewModel model)
+        {
+            Debug.WriteLine(model);
+            return default(IHttpActionResult);
+        }
+    }
+}
---
+++ TestWebApp/Controllers/SessionPostViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TestWebApp.Controllers
+{
+    public class SessionPostViewModel
+    {
+        public DateTime Date { get; set; }
+        public int NoOfSessions { get; set; }
+    }
+}
